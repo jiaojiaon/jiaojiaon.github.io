d78d58a7da62d27874914de88257c670df5a0685
jiaojiaon <jiaojiaon@gmail.com>
2022-10-14 21:35:58 -0400

    upload text form project

--- TextFormProject/textForm.js
+++ TextFormProject/textForm.js
@@ -1,13 +1,83 @@
-const name = document.querySelector('#name');
-const backgroundColor = document.querySelector('#backgroundColor');
-const textColor = document.querySelector('#textColor');
-const size = document.querySelector('#size');
-const reset = document.querySelector('#reset')
+//const name = document.querySelector('#name');
+//const backgroundColor = document.querySelector('#backgroundColor');
+//const textColor = document.querySelector('#textColor');
+//const size = document.querySelector('#size');
+//const reset = document.querySelector('#reset')
 
+//function for reset 
 function reset(){
     const resetname = document.getElementById('name');
     resetname.style.color = "black";
-    resetname.style.fontSize = "12";
-    resetname = "";
+    resetname.style.fontSize = "12px";
+    resetname.value = "";
     resetname.style.background = "white";
 }
+
+//function for change Text Color
+let textColor;
+const defaultTextColor = "black";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    textColor = document.querySelector("#textColor");
+    textColor.value = defaultTextColor;
+    textColor.addEventListener("input", updateFirst, false);
+    textColor.addEventListener("change", updateAll, false);
+    textColor.select();
+}
+
+function updateFirst(event) {
+  const name = document.querySelector("#name");
+  if (name) {
+    name.style.color = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  document.querySelectorAll("name").forEach((name) => {
+    name.style.color = event.target.value;
+  });
+}
+//function for change background color 
+let backgroundColor;
+const defaultBackgroundColor = "white";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    textColor = document.querySelector("#backgroundColor");
+    textColor.value = defaultBackgroundColor;
+    textColor.addEventListener("input", updateFirst, false);
+    textColor.addEventListener("change", updateAll, false);
+    textColor.select();
+}
+
+function updateFirst(event) {
+  const name = document.querySelector("#name");
+  if (name) {
+    name.style.background = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  document.querySelectorAll("name").forEach((name) => {
+    name.style.background = event.target;
+  });
+}
+
+//function for change front size 
+function changesize(){
+    const name = document.getElementById('name');
+    const size = document.getElementById('size').value;
+    name.style.fontSize = size + "px";
+}
+
+//function for the text display in different fonts
+function changefront(obj){
+    const name = document.getElementById('name');
+    name.style.fontFamily = obj.value; 
+
+    
+}
+

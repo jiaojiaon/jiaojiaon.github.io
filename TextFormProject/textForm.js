97af3ba4175b35948f4d65a1420164d3d053b8ec
jiaojiaon <jiaojiaon@gmail.com>
2022-11-02 18:49:55 -0400

    update textform  project

--- TextFormProject/textForm.js
+++ TextFormProject/textForm.js
@@ -1,77 +1,32 @@
-//function for reset 
-function reset(){
-    const resetname = document.getElementById('name');
-    resetname.style.color = "black";
-    resetname.style.fontSize = "12px";
-    resetname.value = "";
-    resetname.style.background = "white";
-}
+const name = document.querySelector("#name");
 
 //function for change Text Color
-let textColor;
-const defaultTextColor = "black";
-
-window.addEventListener("load", startup, false);
-
-function startup() {
-    textColor = document.querySelector("#textColor");
-    textColor.value = defaultTextColor;
-    textColor.addEventListener("input", updateFirst, false);
-    textColor.addEventListener("change", updateAll, false);
-    textColor.select();
+function changeTColor(){
+   var textcolor = document.getElementById('textcolor');
+    name.style.color = textcolor.value;
 }
 
-function updateFirst(event) {
-  const name = document.querySelector("#name");
-  if (name) {
-    name.style.color = event.target.value;
-  }
-}
-
-function updateAll(event) {
-  document.querySelectorAll("name").forEach((name) => {
-    name.style.color = event.target.value;
-  });
-}
 //function for change background color 
-let backgroundColor;
-const defaultBackgroundColor = "white";
-
-window.addEventListener("load", startup, false);
-
-function startup() {
-    backgroundColor = document.querySelector("#backgroundColor");
-    backgroundColor.value = defaultBackgroundColor;
-    backgroundColor.addEventListener("input", updateFirst, false);
-    backgroundColor.addEventListener("change", updateAll, false);
-    backgroundColor.select();
-}
-
-function updateFirst(event) {
-  const name = document.querySelector("#name");
-  if (name) {
-    name.style.background = event.target.value;
-  }
-}
-
-function updateAll(event) {
-  document.querySelectorAll("name").forEach((name) => {
-    name.style.background = event.target;
-  });
+function changeBgColor(){
+  var bgcolor = document.getElementById('backgroundColor');
+  name.style.backgroundColor = bgcolor.value;
 }
 
 //function for change front size 
 function changesize(){
-    const name = document.getElementById('name');
     const size = document.getElementById('size').value;
     name.style.fontSize = size + "px";
 }
 
 //function for the text display in different fonts
 function changefront(obj){
-    const name = document.getElementById('name');
     name.style.fontFamily = obj.value; 
-
-    
 }
 
+//function for reset 
+function reset(){
+    name.style.color = "black";
+    name.style.fontSize = "12px";
+    name.value = "";
+    name.style.background = "white";
+}
